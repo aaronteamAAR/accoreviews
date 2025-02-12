683b3a1a5c5e410892dd45b8c81fc9037bcb682b
Quietonset <davidakachi9@gmail.com>
2025-02-12 12:34:24 +0100

    replaced images and dummy data

--- src/components/SideBarDrawer.tsx
+++ src/components/SideBarDrawer.tsx
@@ -39,48 +39,51 @@ const SidebarDrawer = () => {
         <i className="fa-solid fa-bars text-2xl hover:text-[var(--color-secondary-2)]"></i>
       </Button>
 
-      <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
-        <DrawerOverlay />
+      <div>
+        <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+          <DrawerOverlay />
 
-        <DrawerContent 
-          bg="white" 
-          width={'48'} // Full width on small screens, 50% width on large screens
-        >
-            <DrawerHeader>
-                <Flex align={'center'} gap={2} justify={'space-between'} width={'full'} margin={20}>
-                <div className='h-10 w-3/4 border border-blackAlpha-500 flex items-center gap-2 px-2.5 bg-white hover:border-gray-600'>
-                  <i className="fa-solid fa-magnifying-glass text-xl"></i>
-                  <input 
-                  type="text" 
-                  placeholder='Search'
-                  className='outline-none w-full placeholder:text-gray-400 text-gray-500 text-sm capitalize'
-                  />
+          <DrawerContent 
+            bg="white" 
+            width={'container.sm'} // Full width on small screens, 50% width on large screens
+          >
+              <DrawerHeader>
+                  <Flex align={'center'} gap={2} justify={'space-between'} width={'full'} margin={20}>
+                  <div className='h-10 w-3/4 border border-blackAlpha-500 flex items-center gap-2 px-2.5 bg-white hover:border-gray-600'>
+                    <i className="fa-solid fa-magnifying-glass text-xl"></i>
+                    <input 
+                    type="text" 
+                    placeholder='Search'
+                    className='outline-none w-full placeholder:text-gray-400 text-gray-500 text-sm capitalize'
+                    />
+                  </div>
+                    <DrawerCloseButton size='xl'/>
+                  </Flex>
+              </DrawerHeader>
+
+            <DrawerBody>
+              <ul className="space-y-5">
+                {links.map(link => (
+                  <li key={link.id} className="text-[var(--color-text)] h-10 w-40 px-3 flex items-center">
+                    <a href={link.link}>{link.text}</a>
+                  </li>
+                ))}
+              </ul>
+
+              <div className="mx-10 sm:mx-5 s:mx-5 border-b border-[var(--color-secondary-2)]">
+                <div className="flex items-center gap-3 my-3 text-xl text-[var(--color-text)]">
+                    {icons.map((icon, index) => (
+                        <span key={index} className="h-6 w-6 hover:text-[var(--color-secondary-2)] hover:cursor-pointer">
+                            <a href={icon.link}><i className={icon.iconClass}></i></a>
+                        </span>
+                    ))}
                 </div>
-                  <DrawerCloseButton size='xl'/>
-                </Flex>
-            </DrawerHeader>
+            </div>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      </div>
 
-          <DrawerBody className="w-[50%]">
-            <ul className="space-y-5">
-              {links.map(link => (
-                <li className="text-[var(--color-text)] h-10 w-40 px-3 flex items-center">
-                  <a key={link.id} href={link.link}>{link.text}</a>
-                </li>
-              ))}
-            </ul>
-
-            <div className="mx-10 sm:mx-5 s:mx-5 border-b border-[var(--color-secondary-2)]">
-              <div className="flex items-center gap-3 my-3 text-xl text-[var(--color-text)]">
-                  {icons.map(icon => (
-                      <span className="h-6 w-6 hover:text-[var(--color-secondary-2)] hover:cursor-pointer">
-                          <a href={icon.link}><i className={icon.iconClass}></i></a>
-                      </span>
-                  ))}
-              </div>
-          </div>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
     </>
   );
 };
